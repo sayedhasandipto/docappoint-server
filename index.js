aa5d83ead5db716099f55dd2788e594691cc2a2e
Sayed Hasan Dipto <sayedhasandipto@gmail.com>
2026-06-21 18:38:43 +0600

    Fix CORS issue for Vercel domain

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8000;
 
 // ─── Middleware ────────────────────────────────────────────────────────────────
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://docappoint.vercel.app'],
+    origin: ['http://localhost:3000', 'https://docappoint.vercel.app', 'https://docappointbd.vercel.app'],
     credentials: true,
 }));
 app.use(express.json());
